acb0b0afb5a2c5f010a9e1e93cea3269bb362978
Enzo Ribeiro <enzorib.dev@gmail.com>
2026-07-31 11:02:58 -0300

    feat: add psolf import

--- petrou/optimization/__init__.py
+++ petrou/optimization/__init__.py
@@ -4,6 +4,7 @@ from petrou.optimization.search_space import SearchSpace, VariableDef
 from petrou.optimization.sa import simulated_annealing
 from petrou.optimization.exhaustive import exhaustive_search
 from petrou.optimization.pso import PSO, InertiaRegistry
+from petrou.optimization.psolf import PSOLF
 
 __all__ = [
     "SearchSpace",
@@ -12,4 +13,5 @@ __all__ = [
     "exhaustive_search",
     "PSO",
     "InertiaRegistry",
+    "PSOLF",
 ]
